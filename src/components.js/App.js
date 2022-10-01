ee5987a81aaa59ebb5f15f56049d5d305aa66edd
Snehitha <95093519+PSnehitha@users.noreply.github.com>
2022-10-01 11:05:36 +0530

    clock

--- src/components.js/App.js
+++ src/components.js/App.js
@@ -1,12 +1,16 @@
-import "./cstyles.css";
-//import './styles.css';
-import ChangeColors from "./ChangeColors";
+//import "./cstyles.css";
+import "./styles.css";
+//import ChangeColors from "./ChangeColors";
 //import CricScore from "./CricScore";
 //import MyCart from "./MyCart";
+import DigitalClock from "./DigitalClock";
 function App() {
   return (
-    <div>
-      <ChangeColors />
+    //<div>
+    //<ChangeColors />
+    //</div>
+    <div style={{ width: "90%", margin: "0 auto" }}>
+      <DigitalClock />
     </div>
   );
 }
